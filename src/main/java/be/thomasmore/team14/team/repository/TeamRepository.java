94894e348fc1466ef3e94c6970abca416ae76e71
unknown <r0706810@student.thomasmore.be>
2019-10-25 11:13:02 +0200

    Teamrepository changed

--- src/main/java/be/thomasmore/team14/team/repository/TeamRepository.java
+++ src/main/java/be/thomasmore/team14/team/repository/TeamRepository.java
@@ -2,6 +2,11 @@ package be.thomasmore.team14.team.repository;
 
 import be.thomasmore.team14.team.entity.Team;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 public interface TeamRepository extends MongoRepository<Team, String> {
+    List<Team> findTeamByTeamId(@Param("id") Integer id);
+    List<Team> findTeamByGameId(@Param("gameid") Integer gameId);
+    List<Team> findTeamByOwnerId(@Param("ownerid") Integer ownerId);
 }
